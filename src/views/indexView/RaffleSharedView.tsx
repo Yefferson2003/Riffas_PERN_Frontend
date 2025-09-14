e9f7c0c1d79ae5e8c870175e294c05479fa41705
GeoApp-Co <geoaseoapp@gmail.com>
2025-09-13 21:00:02 -0500

    Update 12/09/2025 shared URL

--- src/views/indexView/RaffleSharedView.tsx
+++ src/views/indexView/RaffleSharedView.tsx
@@ -45,20 +45,27 @@ function RaffleSharedView() {
             <section className="flex flex-col w-full pb-10 text-center lg:flex-col *:bg-white *:p-4 gap-5 *:rounded-xl">
             {/* INFO PRINCIPAL */}
             <div className="space-y-4">
+
+                {/* BANNER */}
+                <div>
+                <img
+                    className="object-cover w-full rounded-lg lg:h-48"
+                    src={
+                    isSmallDevice
+                        ? raffle?.banerMovileImgUrl || "/banner_default.jpg"
+                        : raffle?.banerImgUrl || "/banner_default.jpg"
+                    }
+                    alt="banner rifa"
+                />
+                </div>
                 <div className="flex flex-col items-center lg:justify-between lg:flex-row">
                 <div className="flex flex-col items-center lg:items-start">
-                    <p className="mt-1 text-sm italic text-gray-600">
-                    Información general de la rifa
-                    </p>
                     <h2 className="text-2xl font-bold lg:text-3xl text-azul">
                     {raffle.name}
                     </h2>
                 </div>
 
-                <div className="text-lg text-center text-gray-700 lg:text-right">
-                    <p className="font-semibold text-azul">Responsable</p>
-                    <p>{raffle.nameResponsable}</p>
-                </div>
+
                 </div>
 
                 {/* DESCRIPCIÓN */}
@@ -73,18 +80,6 @@ function RaffleSharedView() {
                 {/* FECHAS */}
                 <div className="grid gap-4 text-center md:grid-cols-3">
                 <div className="p-3 rounded-lg bg-slate-100">
-                    <p className="text-lg font-bold text-azul">Inicio</p>
-                    <p className="text-gray-700">
-                    {formatDateTimeLarge(raffle.startDate)}
-                    </p>
-                </div>
-                <div className="p-3 rounded-lg bg-slate-100">
-                    <p className="text-lg font-bold text-azul">Límite de Compra</p>
-                    <p className="text-gray-700">
-                    {formatDateTimeLarge(raffle.editDate)}
-                    </p>
-                </div>
-                <div className="p-3 rounded-lg bg-slate-100">
                     <p className="text-lg font-bold text-azul">Fecha del Sorteo</p>
                     <p className="text-gray-700">
                     {formatDateTimeLarge(raffle.playDate)}
@@ -92,19 +87,6 @@ function RaffleSharedView() {
                 </div>
                 </div>
 
-                {/* BANNER */}
-                <div>
-                <p className="mb-2 text-lg font-bold text-azul">Banner</p>
-                <img
-                    className="object-cover w-full rounded-lg lg:h-48"
-                    src={
-                    isSmallDevice
-                        ? raffle?.banerMovileImgUrl || "/banner_default.jpg"
-                        : raffle?.banerImgUrl || "/banner_default.jpg"
-                    }
-                    alt="banner rifa"
-                />
-                </div>
             </div>
 
             {token && awards && (
